0419d7eb4ce0696911b642f3b58b01979fafc711
Ryan Page <ryanapage@gmail.com>
2024-12-29 15:02:37 -0800

    #1 add basic swagger support

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
@@ -7,11 +8,13 @@ export class AuthController {
     constructor(private authService: AuthService) {}
 
     @Post('signup')
+    @ApiOperation({ summary: 'Sign up' })
     async signUp(@Body('email') email: string, @Body('password') password: string) {
         await this.authService.signUp(email, password);
     }
 
     @Post('mobile/signin')
+    @ApiOperation({ summary: 'Sign in' })
     @HttpCode(200)
     async mobileSignIn(@Body('email') email: string, @Body('password') password: string) {
         return this.authService.signIn(email, password, '30d');
--- server/src/users/dtos/create-user.dto.ts
+++ server/src/users/dtos/create-user.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
     @IsString()
     @IsNotEmpty()
+    @ApiProperty()
     id: string;
 
     @IsEmail()
     @IsNotEmpty()
+    @ApiProperty()
     email: string;
 }
