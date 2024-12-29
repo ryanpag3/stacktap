a5731aa67c0410cb3ad645e2104fa6fbf34637b2
Ryan Page <ryanapage@gmail.com>
2024-12-29 10:17:25 -0800

    #1 make linter happy

--- server/src/users/dtos/create-user.dto.ts
+++ server/src/users/dtos/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
     @IsString()
