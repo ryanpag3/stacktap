d818bec8f570936d37f6d5f9cb5649ba2e38b8dc
Ryan Page <ryanapage@gmail.com>
2024-12-29 13:52:19 -0800

    #1 enable user service unit tests

--- server/src/users/users.service.spec.ts
+++ server/src/users/users.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './user.entity';
+
+const mockUserRepositoryFactory = jest.fn(() => ({
+  find: jest.fn(entity => [entity]),
+}));
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useFactory: mockUserRepositoryFactory
+        }
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
