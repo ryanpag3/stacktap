40d11fc672e12fe956bc633d831465a17fdc7efc
Ryan Page <ryanapage@gmail.com>
2024-12-29 14:01:14 -0800

    #1 add create and find user unit tests

--- server/src/users/users.service.spec.ts
+++ server/src/users/users.service.spec.ts
@@ -2,9 +2,18 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 const mockUserRepositoryFactory = jest.fn(() => ({
-  find: jest.fn(entity => [entity]),
+  create: jest.fn(entity => entity),
+  save: jest.fn(entity => entity),
+  find: jest.fn(({ where: { email }}) => {
+    const user = new User();
+    user.email = email;
+    user.id = 'id';
+    user.password = 'password';
+    return [user];
+  }),
 }));
 
 describe('UsersService', () => {
@@ -26,5 +35,30 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should be defined', () => {
+      expect(service.create).toBeDefined();
+    });
+
+    it('should return a CreateUserDto', async () => {
+      const result = await service.create('test', 'test');
+      expect(result).toBeInstanceOf(CreateUserDto);
+      expect(result).toHaveProperty('email');
+      expect(result).toHaveProperty('id');
+    });
+  });
+
+  describe('findByEmail', () => {
+    it('should be defined', () => {
+      expect(service.findByEmail).toBeDefined();
+    });
+
+    it('should return a User', async () => {
+      const result = await service.findByEmail('test');
+      expect(result).toBeInstanceOf(User);
+      expect(result.email).toBe('test');
+    });
   });
 });
